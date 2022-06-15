f33d2ce7597f0c37f0a5a6d6a191e262a4932236
venkat <venkat09docs@gmail.com>
2022-06-15 18:27:50 +0530

    Update NumericApplicationTests.java

--- src/test/java/com/devsecops/NumericApplicationTests.java
+++ src/test/java/com/devsecops/NumericApplicationTests.java
@@ -37,7 +37,7 @@ public class NumericApplicationTests {
     @Test
     public void smallerThanOrEqualToFiftyMessage() throws Exception {
         this.mockMvc.perform(get("/compare/49")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(""));
+                .andExpect(content().string("Smaller than or equal to 50"));
     }
 
     @Test
